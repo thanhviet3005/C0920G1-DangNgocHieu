6c67c8665ac6fe9921ab032dcddb3297e4da7fd3
thanhviet3005 <thanhviet3005@gmail.com>
2020-09-29 14:16:44 +0700

    hi

---
+++ case_study/yeu_cau_1/cs_task_1_JS.js
@@ -0,0 +1,37 @@
+
+function displayInfCus(){
+    let t1 ;
+    let totalOr ;
+
+    let nameCus = document.getElementById("nameC").value ;
+    let identifyCus = document.getElementById("identifyC").value ;
+    // let emailCus = document.getElementById("emailC").value ;
+    // let addressCus = document.getElementById("addressC").value ;
+    // let classCus = document.getElementById("classC").value ;
+    // let typeCus = document.getElementById("typeC").value ;
+    // let levelCus = document.getElementById("levelC").value ;
+    // let quantityCus = document.getElementById("quantityC").value ;
+    // let rentDayCus = document.getElementById("rentDayC").value ;
+    // let saleOffCus = document.getElementById("saleoffC").value ;
+
+    // if (typeCus= "Vilas")
+    // {t1 = 500 ; }
+    // else if (typeCus= "House")
+    // {t1 = 300 ; }
+    // else
+    // {t1 = 100 ;}
+    //
+    // totalOr= t1 * quantityCus * rentDayCus * ( 1- saleOffCus/100) ;
+
+    document.getElementById("number1").innerText = nameCus ;  //id="number1"
+    document.getElementById("number2").innerHTML = identifyCus ;  //id="number2"
+    // document.getElementById("number3").innerHTML = emailCus ;  //id="number3"
+    // document.getElementById("number4").innerHTML = addressCus ; //id ="number4"
+    // document.getElementById("number5").innerHTML = classCus ; //id ="number5"
+    // document.getElementById("number6").innerHTML = typeCus ; //id ="number6"
+    // document.getElementById("number7").innerHTML = levelCus ; //id ="number7"
+    // document.getElementById("number8").innerHTML = quantityCus ; //id ="number8"
+    // document.getElementById("number9").innerHTML = rentDayCus ; //id ="number9"
+    // document.getElementById("number10").innerHTML = saleOffCus ; //id ="number10"
+    // document.getElementById("result1").innerHTML = totalOr ; // id = "result1"
+}
